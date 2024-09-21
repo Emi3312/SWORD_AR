5d566296ff731223425c5e6ad4689b86c6077453
Emi3312 <113268254+Emi3312@users.noreply.github.com>
2024-09-21 16:43:22 -0600

    AR_Sword

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2075752.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196786803, 196786804, 196786827, 196786807, 196786808, 196786809, 196786810, 196786811, 196786813, 196786814, 196786815, 196786816, 196786818, 196786819, 196786842, 196787295 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196786824/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196786825/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196786823/1/ammo.js', 'preload' : true},
+];
